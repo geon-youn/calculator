344e57b556711178fca35e5665ec69ab7600ccf7
Geon Youn <geon.youn@outlook.com>
2023-01-18 22:10:10 -0500

    Implement logic

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const opButtons = document.querySelectorAll('.op');
+const delButton = document.querySelector('#del');
+const eqButton = document.querySelector('#eq');
 const symButtons = document.querySelectorAll('.sym');
 const numButtons = document.querySelectorAll('.num');
 const input = document.querySelector('#calc-input');
@@ -24,48 +26,91 @@ function operate(f, a, b) {
     return f(a, b);
 }
 
-opButtons.forEach(button => {
+function doOperation(putToOut) {
+    const parts = input.textContent.split(' ');
+    parts[0] = Number(parts[0]);
+    parts[2] = Number(parts[2]);
+    let ret = '';
+    switch (parts[1]) {
+        case '+':
+            ret = add(parts[0], parts[2]);
+            break;
+        case '-':
+            ret = subtract(parts[0], parts[2]);
+            break;
+        case '÷':
+            ret = divide(parts[0], parts[2]);
+            break;
+        case '×':
+            ret = multiply(parts[0], parts[2]);
+            break;
+    }
+    if (putToOut) {
+        output.textContent = ret;
+    }
+    else {
+        input.textContent = ret;
+        output.textContent = '';
+    }
+}
+
+symButtons.forEach(button => {
     button.addEventListener('click', e => {
-        if (input.textContent === '|') {
+        if (output.textContent !== '') {
+            input.textContent = output.textContent;
+            output.textContent = '';
+        }
+        else if (input.textContent === '|') {
             return;
         }
+        if (input.textContent.split(' ').length === 3) {
+            doOperation(false);
+        }
         switch (e.target.id) {
-            case 'del':
-                let amount = 1;
-                if (input.textContent.charAt(input.textContent.length - 1) === ' ') {
-                    amount = 3;
-                }
-                input.textContent = input.textContent.substring(0, input.textContent.length - amount);
-                if (input.textContent.length === 0) {
-                    input.textContent = '|';
-                }
-                break;
             case 'div':
                 input.textContent += ' ÷ ';
-                refreshDot();
                 break;
             case 'mul':
                 input.textContent += ' × ';
-                refreshDot();
                 break;
             case 'sub':
                 input.textContent += ' - ';
-                refreshDot();
                 break;
             case 'add':
                 input.textContent += ' + ';
-                refreshDot();
-                break;
-            case 'eq':
                 break;
         }
     });
 });
 
+delButton.addEventListener('click', e => {
+    if (input.textContent === '|') {
+        return;
+    }
+    let amount = 1;
+    if (input.textContent.charAt(input.textContent.length - 1) === ' ') {
+        amount = 3;
+    }
+    input.textContent = input.textContent.substring(0, input.textContent.length - amount);
+    if (input.textContent.length === 0) {
+        input.textContent = '|';
+    }
+});
+
+eqButton.addEventListener('click', e => {
+    if (input.textContent.split(' ').length === 3) {
+        doOperation(true);
+    }
+    else if (!input.textContent.includes(' ') && input.textContent !== '|') {
+        output.textContent = input.textContent;
+    }
+});
+
 numButtons.forEach(button => {
     button.addEventListener('click', e => {
-        if (input.textContent === '|') {
+        if (input.textContent === '|' || output.textContent !== '') {
             input.textContent = '';
+            output.textContent = '';
         }
         let append = '';
         switch (e.target.id) {
