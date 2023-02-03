75f1a534a6615d0393823081cb0c2aa055da5e67
Geon Youn <geon.youn@outlook.com>
2023-02-03 15:35:42 -0500

    Trying out prettier

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-const opButtons = document.querySelectorAll('.op');
-const delButton = document.querySelector('#del');
-const eqButton = document.querySelector('#eq');
-const symButtons = document.querySelectorAll('.sym');
-const numButtons = document.querySelectorAll('.num');
-const input = document.querySelector('#calc-input');
+const opButtons = document.querySelectorAll(".op");
+const delButton = document.querySelector("#del");
+const eqButton = document.querySelector("#eq");
+const symButtons = document.querySelectorAll(".sym");
+const numButtons = document.querySelectorAll(".num");
+const input = document.querySelector("#calc-input");
 
-setInterval( () => {
-    if (input.textContent === '|') {
-        input.style.opacity = input.style.opacity === '0' ? '1' : '0';
-    }
-    else {
+setInterval(() => {
+    if (input.textContent === "|") {
+        input.style.opacity = input.style.opacity === "0" ? "1" : "0";
+    } else {
         input.style.opacity = 1;
     }
 }, 750);
 
-const output = document.querySelector('#calc-output');
-const body = document.querySelector('body');
+const output = document.querySelector("#calc-output");
+const body = document.querySelector("body");
 
 function add(a, b) {
     return a + b;
@@ -31,7 +30,7 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if (b === 0) {
-        console.error('The fuck man?');
+        console.error("The fuck man?");
         return 0;
     }
     return a / b;
@@ -42,183 +41,189 @@ function operate(f, a, b) {
 }
 
 function doOperation(putToOut) {
-    const parts = input.textContent.split(' ');
+    const parts = input.textContent.split(" ");
     parts[0] = Number(parts[0]);
     parts[2] = Number(parts[2]);
-    let ret = '';
+    let ret = "";
     switch (parts[1]) {
-        case '+':
+        case "+":
             ret = add(parts[0], parts[2]);
             break;
-        case '-':
+        case "-":
             ret = subtract(parts[0], parts[2]);
             break;
-        case '÷':
+        case "÷":
             ret = divide(parts[0], parts[2]);
             break;
-        case '×':
+        case "×":
             ret = multiply(parts[0], parts[2]);
             break;
     }
     ret = String(ret).length > 10 ? ret.toExponential(2) : ret;
     if (putToOut) {
         output.textContent = ret;
-    }
-    else {
+    } else {
         input.textContent = ret;
-        output.textContent = '';
+        output.textContent = "";
     }
 }
 
 function handleSyms(id) {
-    if (!['div', 'mul', 'sub', 'add', '/', '*', '-', '+', 'x', 'X'].includes(id)) {
+    if (
+        !["div", "mul", "sub", "add", "/", "*", "-", "+", "x", "X"].includes(id)
+    ) {
         return;
     }
-    if (output.textContent !== '' && (input.textContent.includes(' ') || input.textContent === '|')) {
+    if (
+        output.textContent !== "" &&
+        (input.textContent.includes(" ") || input.textContent === "|")
+    ) {
         input.style.opacity = 1;
         input.textContent = output.textContent;
-        output.textContent = '';
-    }
-    else if (input.textContent === '|') {
+        output.textContent = "";
+    } else if (input.textContent === "|") {
         return;
     }
-    if (input.textContent.split(' ').length === 3) {
+    if (input.textContent.split(" ").length === 3) {
         doOperation(false);
     }
     switch (id) {
-        case '/':
-        case 'div':
-            input.textContent += ' ÷ ';
+        case "/":
+        case "div":
+            input.textContent += " ÷ ";
             break;
-        case 'x':
-        case 'X':
-        case '*':
-        case 'mul':
-            input.textContent += ' × ';
+        case "x":
+        case "X":
+        case "*":
+        case "mul":
+            input.textContent += " × ";
             break;
-        case '-':
-        case 'sub':
-            input.textContent += ' - ';
+        case "-":
+        case "sub":
+            input.textContent += " - ";
             break;
-        case '+':
-        case 'add':
-            input.textContent += ' + ';
+        case "+":
+        case "add":
+            input.textContent += " + ";
             break;
     }
 }
 
-symButtons.forEach(button => {
-    button.addEventListener('click', e => handleSyms(e.target.id));
+symButtons.forEach((button) => {
+    button.addEventListener("click", (e) => handleSyms(e.target.id));
 });
 
 function handleDel(id) {
-    if (!['Backspace', 'Delete', 'NumLock', 'del'].includes(id)) {
+    if (!["Backspace", "Delete", "NumLock", "del"].includes(id)) {
         return;
     }
-    if (input.textContent === '|') {
+    if (input.textContent === "|") {
         return;
     }
     let amount = 1;
-    if (input.textContent.charAt(input.textContent.length - 1) === ' ') {
+    if (input.textContent.charAt(input.textContent.length - 1) === " ") {
         amount = 3;
     }
-    input.textContent = input.textContent.substring(0, input.textContent.length - amount);
+    input.textContent = input.textContent.substring(
+        0,
+        input.textContent.length - amount
+    );
     if (input.textContent.length === 0) {
-        input.textContent = '|';
+        input.textContent = "|";
     }
 }
 
-delButton.addEventListener('click', e => handleDel(e.target.id));
+delButton.addEventListener("click", (e) => handleDel(e.target.id));
 
 function handleEq(id) {
-    if (!['=', 'eq', 'Enter'].includes(id)) {
+    if (!["=", "eq", "Enter"].includes(id)) {
         return;
     }
-    if (input.textContent.split(' ').length === 3) {
+    if (input.textContent.split(" ").length === 3) {
         doOperation(true);
-    }
-    else if (!input.textContent.includes(' ') && input.textContent !== '|') {
+    } else if (!input.textContent.includes(" ") && input.textContent !== "|") {
         output.textContent = input.textContent;
     }
 }
 
-eqButton.addEventListener('click', e => handleEq(e.target.id));
+eqButton.addEventListener("click", (e) => handleEq(e.target.id));
 
 function handleNum(id) {
-    let append = '';
+    let append = "";
     switch (id) {
-        case '0':
-        case 'zero':
-            append = '0';
+        case "0":
+        case "zero":
+            append = "0";
             break;
-        case '1':
-        case 'one':
-            append = '1';
+        case "1":
+        case "one":
+            append = "1";
             break;
-        case '2':
-        case 'two':
-            append = '2';
+        case "2":
+        case "two":
+            append = "2";
             break;
-        case '3':
-        case 'three':
-            append = '3';
+        case "3":
+        case "three":
+            append = "3";
             break;
-        case '4':
-        case 'four':
-            append = '4';
+        case "4":
+        case "four":
+            append = "4";
             break;
-        case '5':
-        case 'five':
-            append = '5';
+        case "5":
+        case "five":
+            append = "5";
             break;
-        case '6':
-        case 'six':
-            append = '6';
+        case "6":
+        case "six":
+            append = "6";
             break;
-        case '7':
-        case 'seven':
-            append = '7';
+        case "7":
+        case "seven":
+            append = "7";
             break;
-        case '8':
-        case 'eight':
-            append = '8';
+        case "8":
+        case "eight":
+            append = "8";
             break;
-        case '9':
-        case 'nine':
-            append = '9';
+        case "9":
+        case "nine":
+            append = "9";
             break;
-        case '.':
-        case 'dot':
-            const parts = input.textContent.split(' ');
-            if (input.textContent.includes(' ')) {
-                if (parts[2].includes('.')) {
+        case ".":
+        case "dot":
+            const parts = input.textContent.split(" ");
+            if (input.textContent.includes(" ")) {
+                if (parts[2].includes(".")) {
                     break;
                 }
-            }
-            else if (parts[0].includes('.')) {
+            } else if (parts[0].includes(".")) {
                 break;
             }
-            append = '.'
-            break
+            append = ".";
+            break;
         default:
             return;
     }
     input.style.opacity = 1;
-    if (input.textContent === '|') {
-        input.textContent = '';
+    if (input.textContent === "|") {
+        input.textContent = "";
     }
-    if (input.textContent === '|' && output.textContent !== '') {
-        input.textContent = '';
-        output.textContent = '';
+    if (input.textContent === "|" && output.textContent !== "") {
+        input.textContent = "";
+        output.textContent = "";
     }
     input.textContent += append;
 }
 
-numButtons.forEach(button => {
-    button.addEventListener('click', e => { handleNum(e.target.id) });
+numButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        handleNum(e.target.id);
+    });
 });
 
-body.addEventListener('keydown', e => {
+body.addEventListener("keydown", (e) => {
     handleSyms(e.key);
     handleDel(e.key);
     handleEq(e.key);
